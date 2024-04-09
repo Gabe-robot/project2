959852d57700906fac6617c666cc40b53d786b03
FirefighterMichael94 <154257436+FirefighterMichael94@users.noreply.github.com>
2024-04-09 12:11:18 -0400

    Create profile.js could be used to display story after login
    this page can be used for the story as well for keeping track of it. you would make the profile page contain the story might need a new model for it. might not be needed have for patch if needed for everyone.

---
+++ public/JS/profile.js
@@ -0,0 +1,48 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const name = document.querySelector('#story-name').value.trim();
+    const needed_funding = document.querySelector('#story-funding').value.trim();
+    const description = document.querySelector('#story-desc').value.trim();
+  
+    if (name && needed_funding && description) {
+      const response = await fetch(`/api/story`, {
+        method: 'POST',
+        body: JSON.stringify({ name, needed_funding, description }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to create story');
+      }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/story/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to delete story');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new-story-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.story-list')
+    .addEventListener('click', delButtonHandler);
+  
